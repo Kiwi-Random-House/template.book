4f6424c6f9e2485f578b462e80115d2c81ed2b4d
harell <harel@loostiger.com>
2019-12-24 12:01:10 +1300

    install package from description

--- .tic/install.R
+++ .tic/install.R
@@ -1,3 +1,9 @@
 source(".tic/helper-functions.R")
+
+# Package Manager ---------------------------------------------------------
+## Set repo URL
 options(repos = Sys.getenv("repos", getOption("repos")))
 message("Default CRAN mirror snapshot taken on ", gsub("^.*/", "", getOption("repos")))
+## Install requirements
+.install_local_package()
+.uninstall_local_package()
