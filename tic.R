49e6b37c65568ed9c60696da422fce61607cd72f
harell <harel@loostiger.com>
2020-04-12 15:35:52 +1200

    add_step(step_do_push_deploy(path = "./manuscript/_book", commit_message = NULL, commit_paths = "."))

--- tic.R
+++ tic.R
@@ -34,7 +34,8 @@ get_stage("deploy") %>%
     # add_step(step_build_bookdown(input = "index.Rmd", output_format = "all", output_dir = "_book")) %>% 
     add_step(step_build_bookdown(input = "index.Rmd", output_format = NULL, output_dir = "_book")) %>% 
     add_code_step(setwd("..")) %>% 
-    add_code_step(print(fs::dir_ls(recursive = TRUE))) %>%
+    add_code_step(fs::file_delete(".gitignore")) %>%
+    add_code_step(print(fs::dir_ls(recurse = TRUE))) %>%
     add_code_step(fs::dir_delete("./_book")) %>%
     add_code_step(fs::dir_copy("./manuscript/_book", ".", overwrite = TRUE)) %>% 
     add_step(step_do_push_deploy(path = "_book", commit_message = NULL, commit_paths = "."))
