89c48baa68954d7ce6d00b079d80ce3c02ab5e60
harell <harel@loostiger.com>
2020-04-12 13:47:27 +1200

    add_code_step(print(list.dirs())) %>%

--- tic.R
+++ tic.R
@@ -31,6 +31,7 @@ get_stage("before_deploy") %>%
 get_stage("deploy") %>%
     add_code_step(setwd("./manuscript")) %>% 
     add_step(step_build_bookdown(input = "", output_format = "all", output_dir = "_book")) %>% 
+    add_code_step(print(list.dirs())) %>% 
     add_step(step_do_push_deploy())
 
 # Stage: After Deploy -----------------------------------------------------
